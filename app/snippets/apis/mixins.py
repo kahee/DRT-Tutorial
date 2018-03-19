09dfe78a78e01d3f90c8678f315bafc97780ff3e
kahee <yuygh131@gmail.com>
2018-03-20 00:57:50 +0900

    Mixins로 SnippetList, Detail구현

---
+++ app/snippets/apis/mixins.py
@@ -0,0 +1,35 @@
+from rest_framework import mixins, generics
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    # 입력의 유효성을 검사하고 직렬화를 해제하고 출력을 직렬화하는 데 사용되는 serializer 클래스입니다.
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
