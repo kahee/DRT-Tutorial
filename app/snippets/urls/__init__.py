7713e75304d243e60fe1898b160e89fd1396c276
kahee <yuygh131@gmail.com>
2018-03-21 00:44:19 +0900

    SnippetViewSet 및 as_view()를 사용한 url 연결

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
 
-
 urlpatterns = [
     path('api-view/', include('snippets.urls.api_view')),
     path('mixins/', include('snippets.urls.mixins')),
     path('generic/', include('snippets.urls.generic')),
-
-
+    path('viewsets/', include('snippets.urls.viewsets')),
 ]
